1a85cb2275902f7f4f1f4debed92232539509436
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2021-03-09 17:51:10 -0500

    plotting: parameters tuning

--- Stability/scripts/plot.jl
+++ Stability/scripts/plot.jl
@@ -19,7 +19,7 @@ plot_col(df :: DataFrame, col :: Symbol, df_name :: String = "noname", prefix ::
            cols(col),
            :stable,
            c=cgrad([:blue, :red]),
-           nbins=(60,30),
+           nbins=(200,20),
            size=(1200,800),
            legend=false,
            title=df_name,
