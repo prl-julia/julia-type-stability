d7532c75003263b20b55c82dcd36a7971b89dee2
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2021-03-20 20:44:41 +0100

    plots: make collage in Julia; color scheme := viridis

--- Stability/scripts/plot.jl
+++ Stability/scripts/plot.jl
@@ -15,19 +15,16 @@ using StatsPlots
 plot_col(df :: DataFrame, col :: Symbol, df_name :: String = "noname", prefix :: String = "") = begin
     mi=max(1, minimum(df[!, col]))
     ma=maximum(df[!, col])
+    @info "About to plot $df_name"
     @df df histogram2d(
            cols(col),
            :stable,
            c=cgrad(:viridis),
+           cb = true,
            nbins=(20,10),
-           size=(1200,800),
-           legend=false,
            title=df_name,
            xlim=[mi,ma],
            ylim=[0,1.2])
-    out=joinpath(prefix,"$(df_name)-$(col).png")
-    @info "About to store the plot in $out"
-    savefig(out)
 end
 
 plot_pkg(pkg :: String, col :: Symbol = :size, odir :: String = ".", idir :: String = ".") = begin
@@ -39,16 +36,20 @@ end
 
 # Plot with plot_col for every package in the list stored in pkgs_file
 # Assumes: every package name corespondes to a dir in the current dir
-plot_all_pkgs(pkgs_file :: String, col :: Symbol = :size) = begin
+plot_all_pkgs(pkgs_file :: String, odir :: String = ".", col :: Symbol = :size) = begin
     isfile(pkgs_file) || (@error "Invalid package list file $pkgs_file"; return)
 
-    odir="by-$col"
     #!isdir(odir) || rm(odir, force=true, recursive=true) # more reproducible
     #mkdir(odir)
     isdir(odir) || mkdir(odir)
 
     pkgs = readlines(pkgs_file)
+    plots = []
     for p in pkgs
-        plot_pkg(p, col, odir)
+        push!(plots, plot_pkg(p, col, odir))
     end
+    plot(plots..., layout = (2, 5), size=((1200+900)*5,800*2))
+    out=joinpath(odir,"all-by-$(col).png")
+    savefig(out)
+
 end
