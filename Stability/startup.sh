6cc9e567ba248ef48c1d2f6b2df4b54672116350
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2021-03-28 16:14:44 +0200

    fix startup.sh to correctly find startup.jl & not run in parallel
    it seems -p don't like absence of Distributed

--- Stability/startup.sh
+++ Stability/startup.sh
@@ -2,4 +2,4 @@
 # NOTE: Run from the Stability directory!
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 STABILITY_HOME="$DIR"
-DEV=YES PAR=NO STABILITY_HOME=$STABILITY_HOME julia -p 32 -L ./startup.jl
+DEV=YES PAR=NO STABILITY_HOME=$STABILITY_HOME julia -L "$DIR/startup.jl"
