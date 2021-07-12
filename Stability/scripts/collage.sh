1d2634a1e2fc62abe2a5701437cb155a498f528d
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2021-07-13 01:28:00 +0200

    cleanup

--- Stability/scripts/collage.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [[ -z $1 && -z $2 ]]; then
-    echo "Make sure to pass the column name (e.g. size) and grid size (e.g 4x2)"
-    exit 1;
-fi
-col="$1"  # e.g. size
-grid="$2" # e.g. 4x2
-montage "by-$col/*.png" -geometry 1200x800+0+0 -tile "$grid" "all-by-$col.png"
