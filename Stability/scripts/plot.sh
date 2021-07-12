9d78254c8085aaeded6d4913ad8da0790c565b4f
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2021-07-13 01:52:11 +0200

    rm obsolete plot.sh

--- Stability/scripts/plot.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-if [[ -z ${1+x} ]]; then
-    echo "Error: Make sure to pass filename with list of packages. Bye!"
-    exit 1;
-fi
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-julia -O0 -L "$DIR/plot.jl" -e "plot_all_pkgs(\"$1\")"
