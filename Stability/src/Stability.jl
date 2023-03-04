163676ace23a322ccaed6990a0830a1d8228c87e
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-03-04 10:50:34 -0500

    better versioning of packages (see below)
    - allow `proc_package.sh` to skip version parameter (and allow to pass it
      w/o double quotes around package-version pair)
    - if both: version is provided and Manifest file is found chack that
      versions agree

--- Stability/src/Stability.jl
+++ Stability/src/Stability.jl
@@ -389,25 +389,59 @@ is_blocklisted(modl_proccessed :: Module, modl_mi :: Module) = begin
         false
 end
 
+# store_cur_version :: String -> IO ()
+# Store the version of the given package that we just processed into a file
+# named "version.txt"
+store_cur_version(pkg::String) = begin
+    ver  = installed_pkg_version(pkg)
+    fname= "version.txt"
+    write(fname, "$ver")
+    @info "[Stability] Write down $pkg version to $fname"
+end
+
+# installed_pkg_version :: String ->IO String
+# Will querry current environment for the version of given package
+#
+installed_pkg_version(pkg::String) = begin
+    deps = collect(values(Pkg.dependencies()))
+    i    = findfirst(i -> i.name == pkg, deps)
+    if i === nothing
+        nothing
+    else
+        string(deps[i].version)
+    end
+end
+
+macro myinfo(pkgtag, msg)
+    esc(:( @info ("[Stability] [Package: " * $pkgtag * "] " * $msg ) ))
+end
+
 #
 #  Section: Stats for type stability, package level
 #
 
-# package_stats: (pakg: String) -> IO ()
+# package_stats: (pakg: String, ver: String) -> IO ()
 #
-# Runs stability analysis for the package `pakg` of version `ver`.
-# Results are stored in the following files of the current directory (see also "Side Effects" below):
-# * stability-stats.out
-# * stability-errors.out
-# * stability-stats-per-method.csv
-# * stability-stats-per-instance.csv
+# In the current directory:
+# - runs stability analysis for the package `pakg` of version `ver` (initializing package environment accordingly)
+# - results are stored in the following files of the current directory (see also "Side Effects" below):
+#   * stability-stats.out
+#   * stability-errors.out
+#   * stability-stats-per-method.csv
+#   * stability-stats-per-instance.csv
+#   * $pakg-version.txt (version stamp for future reference)
+#
+# Setting a package version:
+#   The `ver` parameter has been added recently, and has rough corners. E.g. if you call the function in
+#   a directory with a manifest file, we'll process the version specified in the manifest instead of `ver`.
+#   So, be sure to run in an empty dir if you care about the `ver` parameter.
 #
 # Side effects:
-#   In the current directory, creates a temporary environment for this package.
+#   In the current directory, creates a temporary environment for this package, and the resulting files.
 #   Reusing this env shouldn't harm anyone (in theory).
 #
 # Parallel execution:
-#   Possible with the aid of GNU parallel and the tine script in scripts/proc_package_parallel.sh.
+#   Possible e.g. with the aid of GNU parallel and the tiny script in scripts/proc_package_parallel.sh.
 #   It requires a file with a list of packages passed as the first argument.
 #
 # REPL:
@@ -415,13 +449,15 @@ end
 #   and cd into it before calling.
 #
 package_stats(pakg :: String, ver = nothing) = begin
-    start_dir = pwd()
     work_dir  = pwd()
     ENV["STAB_PKG_NAME"] = pakg
     ENV["WORK_DIR"] = work_dir
+    pkgtag = pakg * (ver === nothing ? "" : "@v$ver")
 
-    @info "[Stability] [Package: " * pakg * "] Starting up"
-    # set up and test the package `pakg`
+    #
+    # Set up and test the package `pakg`
+    #
+    @myinfo pkgtag "Starting up"
     try
         Pkg.activate(".") # Switch from Stability package-local env to a temp env
         if isfile("Manifest.toml")
@@ -429,8 +465,17 @@ package_stats(pakg :: String, ver = nothing) = begin
         else
             Pkg.add(name=pakg, version=ver)
         end
-        @info "[Stability] [Package: " * pakg * (ver === nothing ? "" : "@$ver") * "] Added. Now on to testing"
+        # Sanity checking w.r.t Manifest vs $ver parameter
+        iver = installed_pkg_version(pakg)
+        if !(ver === nothing) && ver != iver
+            throw(ErrorException("[Stability] The Manifest file in the current directory declares " *
+                            "a version of the package $pakg other than requested. " *
+                            "Either remove the Manifest or don't supply the version."))
+        end
+        pkgtag = pakg * "@v$iver"
+        @myinfo pkgtag "Added. Now on to testing"
         Pkg.test(pakg)
+        store_cur_version(pakg)
     catch err
         println("Error when running tests for package $(pakg)")
         errio=stderr
@@ -438,10 +483,12 @@ package_stats(pakg :: String, ver = nothing) = begin
         showerror(errio, err, stacktrace(catch_backtrace()))
         println(errio)
     finally
-        cd(start_dir)
         Pkg.activate(dirname(@__DIR__)) # switch back to Stability env
     end
 
+    #
+    # Write down the results
+    #
     resf = joinpath(work_dir, "stability-stats-per-method.txt")
     isfile(resf) || (@error "Stability analysis failed to produce output $resf"; return)
     st =
@@ -455,7 +502,7 @@ package_stats(pakg :: String, ver = nothing) = begin
         eval(Meta.parse(
             open(f-> read(f,String), resf,"r")))
     CSV.write(joinpath(work_dir, "stability-stats-per-instance.csv"), st)
-    @info "[Stability] [Package: " * pakg * "] Results successfully converted to CSV. The package is DONE!"
+    @myinfo pkgtag "Results successfully converted to CSV. The package is DONE!"
 end
 
 end # module
