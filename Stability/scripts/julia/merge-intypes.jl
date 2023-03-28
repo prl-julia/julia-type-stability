4487f83a1a12ecd0b70167e1b427b1401a9c17d4
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-03-28 15:36:25 +0200

    scripts: add merge-intypes.jl

---
+++ Stability/scripts/julia/merge-intypes.jl
@@ -0,0 +1,30 @@
+using CSV, DataFrames, Query
+
+#
+# Merge stability-stats-intypes.csv table summing up occurs field.
+#
+# Run from a root of a pkgs directory, i.e. every subdir corresponds to
+# a package and holds a `stability-stats-intypes.csv` file
+#
+
+
+idir="."
+
+main() = begin
+resdf = DataFrame()
+for package in readdir(idir)
+    isfile(package) && continue
+    in="$idir/$package/stability-stats-intypes.csv"
+    isfile(in) || (@warn "File not found: $in"; continue;)
+    @info "Processing " package
+    resdf = vcat(resdf, CSV.read(in, DataFrame))
+    resdf = @from i in resdf begin
+        @group i.occurs by i.modl, i.tyname into g
+        @select {modl=key(g)[1], tyname=key(g)[2], occurs=sum(g)}
+        @collect DataFrame
+    end
+end
+resdf = resdf |> @orderby_descending(_.occurs) |> DataFrame
+println(resdf)
+CSV.write("merged-intypes.csv", resdf)
+print("Done! Bye!")
